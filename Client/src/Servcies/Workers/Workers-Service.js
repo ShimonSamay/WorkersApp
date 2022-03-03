0e4031a9e9f31f637a5b8aaf8ce68a453471ee18
ShimonSamay <shimonsamay7@gmail.com>
2022-03-03 12:26:41 +0200

    scripts added

--- Client/src/Servcies/Workers/Workers-Service.js
+++ Client/src/Servcies/Workers/Workers-Service.js
@@ -1,4 +1,5 @@
-const BaseUrl = "http://localhost:5000/workers" ;
+const BaseUrl = process.env.NODE_ENV === "production" ? 
+"https://myoffice-mern-app.herokuapp.com/workers" : "http://localhost:5000/workers" ;
 
 export const getAll = async () => {
   return await fetch (`${BaseUrl}`)
--- Server/Server.js
+++ Server/Server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("./DB/Office-DB");
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express() ;
 const port = process.env.PORT ;
 const officeRouter = require("./Routes/Workers-Router")
@@ -17,6 +18,11 @@ app.use(cors());
 app.use("/workers" , passport.authenticate("jwt" , {session:false}), officeRouter );
 app.use("/myapp"  , userRouter )
 
-
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(`Client/build`));
+    app.get("*" , (req , res) => {
+     res.sendFile(path.join(__dirname , "../Client/build" , "index.html" ));
+    })
+}
 
 app.listen(port);
